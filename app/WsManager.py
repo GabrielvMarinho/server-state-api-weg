78c9df0900c80dcd396a05a99e90bb6c7e0cbb2d
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-07-23 14:44:57 -0300

    changed data types for smaller inputs, endpoint to add and endpoint to remove now

--- app/WsManager.py
+++ app/WsManager.py
@@ -1,5 +1,5 @@
 from fastapi import WebSocket
-from structures import BroadcastDTO
+from structures import MacroPost
 from dotenv import load_dotenv
 import uuid
 import traceback
@@ -8,35 +8,57 @@ load_dotenv()
 class WsManager:
     
     clients = {}
-    lastBroadcast = BroadcastDTO()
+    currentStateMacros = []
 
     @staticmethod
     async def new_client_connection(websocket: WebSocket):
         unique_id = str(uuid.uuid4())
         await websocket.accept()
         WsManager.clients[unique_id] = (websocket)
-        await websocket.send_json(WsManager.lastBroadcast.model_dump())
+        await websocket.send_json(WsManager.currentStateMacros)
         try:
             while True:
-                
-                await websocket.receive_text() 
+                await websocket.receive() 
         except:
             del WsManager.clients[unique_id]
             
     @staticmethod
-    async def send_json_or_delete(id, client, broadcastDto):
+    async def send_json_or_delete(id, client):
         try:
-            await client.send_json(broadcastDto.model_dump())
+            await client.send_json(WsManager.currentStateMacros)
         except Exception as e:
             print(e)
             del WsManager.clients[id]
 
     @staticmethod
-    async def broadcast_action(broadcastDto :BroadcastDTO):
+    async def broadcast_new_state():
         try:
             for id, client in list(WsManager.clients.items()):
-                WsManager.lastBroadcast = broadcastDto
-                await WsManager.send_json_or_delete(id, client, broadcastDto)
+                await WsManager.send_json_or_delete(id, client)
         except:
             traceback.print_exc()
+
+
+
+    @staticmethod
+    async def add_to_state(macro: MacroPost):
+        alreadyExists = False
+        for macroInList in WsManager.currentStateMacros:
+            if macroInList["id"] == macro.id:
+                alreadyExists = True
+
+        if(not alreadyExists and len(WsManager.currentStateMacros) <= 6):
+            WsManager.currentStateMacros.append(macro.model_dump())
+            return
+
+        raise Exception("Macro already existant")
+    @staticmethod
+    async def remove_from_state(id: str):
+        print(WsManager.currentStateMacros)
+        for macroInList in list(WsManager.currentStateMacros):
+            if macroInList["id"] == id:
+                WsManager.currentStateMacros.remove(macroInList)
+                return
+        raise Exception("No macro to remove")
+
     
--- app/functions.py
+++ app/functions.py
@@ -1,13 +1,36 @@
-from structures import Broadcast, BroadcastDTO
+from structures import Broadcast, BroadcastFinishMacro
 import os
 from WsManager import WsManager
 import traceback
 
-async def new_broadcast(broadcast: Broadcast):
-    if broadcast.token and broadcast.token==os.getenv("WS_SENDER_KEY"):
-        try:
-            broadcastDto = BroadcastDTO(**broadcast.model_dump())                 
-            await WsManager.broadcast_action(broadcastDto)
-        except:
-            traceback.print_exc()
 
+async def remove_and_broadcast(broadcast :BroadcastFinishMacro):
+    if is_token_valid(broadcast.token):
+        try:
+            await WsManager.remove_from_state(broadcast.id)          
+            await WsManager.broadcast_new_state()
+
+            return {"status":"success"}
+        except:
+            err = traceback.print_exc()
+            return {"status":"fail", "error":err}
+    else:
+        {"status":"fail", "error":"Not valid token"}
+        
+
+async def add_and_broadcast(broadcast :Broadcast):
+    if is_token_valid(broadcast.token):
+        try:
+            await WsManager.add_to_state(broadcast.macro)          
+            await WsManager.broadcast_new_state()
+            return {"status":"success"}
+        except:
+            err = traceback.print_exc()
+            return {"status":"fail", "error":err}
+    else:
+        {"status":"fail", "error":"Not valid token"}
+
+def is_token_valid(token):
+    if token and token==os.getenv("WS_SENDER_KEY"):
+        return True
+    return False
